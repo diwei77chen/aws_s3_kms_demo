7fcf7b9a85420d4cf5b90fd8f1317a507c74b8ae
diwei77chen <diwei77chen@gmail.com>
2021-02-24 16:35:58 +1100

    added openConcurrentDownloads util function

--- utils/index.js
+++ utils/index.js
@@ -1,7 +1,23 @@
-const fs = require("fs-extra");
+/**
+ * write data to file
+ * @param {object} fs NodeJS file system
+ * @param {string} path Path to save the data
+ * @param {*} body The data
+ * return a promise 
+ */
+const writeToFile = (fs, path, body) => fs.outputFile(path, body);
 
-const writeToFile = (path, body) => fs.outputFile(path, body);
+/**
+ * download files concurrently.
+ * @param {object} bPromise Bluebird Promise 
+ * @param {array} keys Array of file paths 
+ * @param {number} concurrency Number of concurrent dowloads
+ * @param {function} downloadRequest The download request
+ * return the final download result for the given file paths 
+ */
+const openConcurrentDownloads = (bPromise, keys = [], concurrency, downloadRequest) => bPromise.map(keys, key => downloadRequest(key), { concurrency })
 
 module.exports = {
-    writeToFile
+    writeToFile,
+    openConcurrentDownloads
 }
