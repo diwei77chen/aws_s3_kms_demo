968c8ed0e1ac5c3d4cbe50d71edccf8515b8dd58
diwei77chen <diwei77chen@gmail.com>
2021-02-24 16:36:20 +1100

    initialised the server

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const http = require("http");
+const dotenv = require("dotenv");
+const { downloadAndSaveFiles }= require('./requests/s3');
+
+const app = express();
+const server = http.createServer(app);
+
+// Load configuration files
+dotenv.config({ path: "./config.env" });
+
+// Body parser middleware
+app.use(express.json());
+
+// Port configuration
+const PORT = process.env.PORT || 6000;
+
+server.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}.`);
+    downloadAndSaveFiles()
+    .then(() => console.log("The files have been downloaded and saved to the project directory."))
+    .catch(err => console.error("Download And Save Files error: ", err))
+})
