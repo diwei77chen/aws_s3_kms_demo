73b1002896ca39519029e4105a02ea0b6955e388
diwei77chen <diwei77chen@gmail.com>
2021-02-24 22:24:21 +1100

    added BUCKET_NAME to AWS_Settings

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const http = require("http");
 const dotenv = require("dotenv");
 const { downloadAndSaveFiles } = require("./requests/aws");
+const AWS_Settings = require("../settings/AWS.json");
 
 const app = express();
 const server = http.createServer(app);
@@ -17,7 +18,7 @@ const PORT = process.env.PORT || 6000;
 
 server.listen(PORT, () => {
   console.log(`Server started on port ${PORT}.`);
-  downloadAndSaveFiles()
+  downloadAndSaveFiles(AWS_Settings.BUCKET_NAME)
     .then(() =>
       console.log(
         "The files have been downloaded and saved to the project directory."
